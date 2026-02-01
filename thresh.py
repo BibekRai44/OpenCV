a6a6faab9cf5f378a342b9744fd017d9b682e54a
Bibek Rai <bibekrai655@gmail.com>
2026-02-01 16:18:51 +0545

    thresholding images

---
+++ thresh.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('photos/dogs.jpg')
+cv.imshow('Dogs',img)
+
+#simple thresholding
+
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow('Gray',gray)
+threshold,thresh = cv.threshold(gray,150,255,cv.THRESH_BINARY)
+cv.imshow('Simple Thresholded',thresh)
+
+#inverse thresholding
+threshold,thresh_inv = cv.threshold(gray,150,255,cv.THRESH_BINARY_INV)
+cv.imshow('Simple Thresholded Inverse',thresh_inv)
+
+#adaptive thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,11,3)
+cv.imshow('Adaptive Thresholding',adaptive_thresh)
+cv.waitKey(0)
